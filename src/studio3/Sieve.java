f6ac294deda34710cadfc989f51984ba1ca34aa9
Minkyu Tak <m.tak@wustl.edu>
2025-09-17 14:10:12 -0500

    studio3

---
+++ src/studio3/Sieve.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Sieve {
+    public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+
+        /*System.out.println("Value of n:");
+        int n = scan.nextInt();
+        int array[] = new int[n-1];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i + 2;
+        }
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i]);
+            System.out.println(" ");
+        }
+        for (int i = 0; i < array.length; i++) {
+            if () */
+        
+        int n = scan.nextInt();
+        boolean [] isPrime = new boolean [n+1];
+            for (int i = 2; i <= n; i++) {
+                isPrime[i] = true;
+            }
+            for (int i = 2; )
+        }
+    }
+}
